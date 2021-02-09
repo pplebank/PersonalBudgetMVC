e00882223e2474ea7cdbc77d8a3250cdde12ea6b
Piotr Plebankiewicz <pplebank@gmail.com>
2021-02-09 15:58:28 +0100

    new feature

--- App/Config.php
+++ App/Config.php
@@ -6,9 +6,9 @@ class Config
 {
 
     const DB_HOST = 'localhost';
-    const DB_NAME = 'mvc';
-    const DB_USER = 'root';
-    const DB_PASSWORD = '';
+    const DB_NAME = 'mybudgetmvc';
+    const DB_USER = 'admin';
+    const DB_PASSWORD = 'password';
     const SHOW_ERRORS = true;
 
 }
