621dcc54729f211d679f292648580f7f7d02164e
Piotr Plebankiewicz <pplebank@gmail.com>
2021-02-11 00:04:16 +0100

    added frontpage and login form on server side

--- App/Config.php
+++ App/Config.php
@@ -7,7 +7,7 @@ class Config
 
     const DB_HOST = 'localhost';
     const DB_NAME = 'mybudgetmvc';
-    const DB_USER = 'admin';
+    const DB_USER = 'Adminserver';
     const DB_PASSWORD = 'password';
     const SHOW_ERRORS = true;
 
---
+++ App/Models/User.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Models;
+
+use PDO;
+
+class User extends \Core\Model
+{
+
+    public $errors = [];
+
+    public function __construct($data)
+    {
+        foreach ($data as $key => $value) {
+            $this->$key = $value;
+        };
+    }
+
+    public function save()
+    {
+        $this->validate();
+        if (empty($this->errors)) {
+
+            $password_hash = password_hash($this->password, PASSWORD_DEFAULT);
+
+            $sql = 'INSERT INTO users (name, email, password_hash)
+            VALUES (:name, :email, :password_hash)';
+
+            $db = static::getDB();
+            $stmt = $db->prepare($sql);
+
+            $stmt->bindValue(':name', $this->name, PDO::PARAM_STR);
+            $stmt->bindValue(':email', $this->email, PDO::PARAM_STR);
+            $stmt->bindValue(':password_hash', $password_hash, PDO::PARAM_STR);
+
+            return $stmt->execute();
+        }
+        return false;
+    }
+
+    public function validate()
+    {
+
+        if ($this->name == '') {
+            $this->errors[] = 'Name is required';
+        }
+
+        if (filter_var($this->email, FILTER_VALIDATE_EMAIL) === false) {
+            $this->errors[] = 'Invalid email';
+        }
+
+        if ($this->emailExists($this->email)) {
+            $this->errors[] = 'email already taken';
+        }
+        if ($this->password != $this->passwordConfirmation) {
+            $this->errors[] = 'Password must match confirmation';
+        }
+
+        if (strlen($this->password) < 6) {
+            $this->errors[] = 'Please enter at least 6 characters for the password';
+        }
+
+        if (preg_match('/.*[a-z]+.*/i', $this->password) == 0) {
+            $this->errors[] = 'Password needs at least one letter';
+        }
+
+        if (preg_match('/.*\d+.*/', $this->password) == 0) {
+            $this->errors[] = 'Password needs at least one number';
+        }
+    }
+
+    protected function emailExists($email)
+    {
+        $sql = 'SELECT * FROM users WHERE email = :email';
+
+        $db = static::getDB();
+        $stmt = $db->prepare($sql);
+        $stmt->bindParam(':email', $email, PDO::PARAM_STR);
+
+        $stmt->execute();
+
+        $result = $stmt->fetch();
+        if ($result === false) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+}
