bd6285918a1c75cf7080128b036477134ba7e54d
Piotr Plebankiewicz <pplebank@gmail.com>
2021-02-12 22:36:54 +0100

    async email checking implemented

--- App/Controllers/Account.php
+++ App/Controllers/Account.php
@@ -7,11 +7,12 @@ use \App\Models\User;
 class Account extends \Core\Controller
 {
 
-  public function validateEmailAction()
-  {
- $is_valid = User::emailExists($_GET['email']);
+    public function validateEmailAction()
+    {
+         $is_valid = User::emailExists($_GET['email']);
 
- header('Content-Type: application/json');
- echo json_encode($is_valid);
-  }
+        header('Content-Type: application/json');
+        echo json_encode($is_valid);
+
+    }
 }
